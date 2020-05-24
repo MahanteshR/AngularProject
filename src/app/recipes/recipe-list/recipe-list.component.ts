926c4ef6c45dedd6fe17d8618de0fec39dd09dae
MahanteshR <mahanteshr17@gmail.com>
2020-05-24 18:02:51 +0530

    Added Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipesModel} from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: RecipesModel[] = [
+    new RecipesModel('Uggani', 'YUMYUM',
+      'https://yummyindiankitchen.com/wp-content/uploads/2015/05/Uggani-or-puffed-rice-recipe-puffed-rice-upma.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IngredientModel} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: IngredientModel[] = [
+    new IngredientModel('Murmure', 20),
+    new IngredientModel('Tomatoes', 30),
+    new IngredientModel('Onion', 10),
+    new IngredientModel('Coriander', 5)
+  ];
   constructor() { }
 
   ngOnInit() {
